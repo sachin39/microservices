bf2134713a3bc9299b7eb5aa8c60418e4e8aefe6
Sachin Kumar <Sachin.kr.39@gmail.com>
2018-07-03 17:41:14 +0530

    updated to work with and deploy on Eureka naming server

--- currency-conversion-service/src/main/java/com/sachin/microservices/feignclient/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/sachin/microservices/feignclient/CurrencyExchangeServiceProxy.java
@@ -10,9 +10,20 @@ import com.sachin.microservices.model.CurrencyconversionBean;
 
 //@FeignClient(name="currency-exchange-service", url="localhost:8000")
 @FeignClient(name = "currency-exchange-service")
-@RibbonClient(name = "currency-exchange-service", configuration = LocalRibbonClientConfiguration.class) // we want to configure what URLs the load for this services has to be
-						  // distributed between. This can be defined in application.properties
 
+
+/*
+ * we want to configure what URLs the load for this services has to be
+ * distributed between. This can be defined in application.properties
+ */
+//@RibbonClient(name = "currency-exchange-service", configuration = LocalRibbonClientConfiguration.class)
+
+
+/*
+ * Commented above line since we want to read servers details, on which
+ * currency-exchange service is running from eureka server.
+ */
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyconversionBean retrieveExchangeValue(@PathVariable("from") String from,
--- currency-exchange-service/src/main/java/com/sachin/startup/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/sachin/startup/CurrencyExchangeServiceApplication.java
@@ -3,6 +3,7 @@ package com.sachin.startup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories("com.sachin.repository")
 @ComponentScan(basePackages="com.sachin")
 @EntityScan("com.sachin.model")
+@EnableDiscoveryClient
 public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
