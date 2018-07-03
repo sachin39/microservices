aec78edce19334119dd2cb816a3a0dc8c02169ff
Sachin Kumar <Sachin.kr.39@gmail.com>
2018-07-03 13:14:28 +0530

    Created and used RibbonClient as client side load balancer

--- currency-conversion-service/src/main/java/com/sachin/microservices/feignclient/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/sachin/microservices/feignclient/CurrencyExchangeServiceProxy.java
@@ -1,13 +1,20 @@
 package com.sachin.microservices.feignclient;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.sachin.microservices.configuration.LocalRibbonClientConfiguration;
 import com.sachin.microservices.model.CurrencyconversionBean;
 
-@FeignClient(name="currency-exchange-service", url="localhost:8000")
+//@FeignClient(name="currency-exchange-service", url="localhost:8000")
+@FeignClient(name = "currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service", configuration = LocalRibbonClientConfiguration.class) // we want to configure what URLs the load for this services has to be
+						  // distributed between. This can be defined in application.properties
+
 public interface CurrencyExchangeServiceProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
-    public CurrencyconversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
+    public CurrencyconversionBean retrieveExchangeValue(@PathVariable("from") String from,
+	    @PathVariable("to") String to);
 }
--- currency-exchange-service/src/main/java/com/sachin/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/sachin/controller/CurrencyExchangeController.java
@@ -1,6 +1,7 @@
 package com.sachin.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +14,20 @@ public class CurrencyExchangeController {
    
     @Autowired
     private ExchangeValueRepository repository;
+    
+    @Autowired
+    private Environment environment;
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 
 	ExchangeValue exchangeValue = repository.findUsingHQL(from, to);
 	
+	exchangeValue.setPort(
+		Integer.parseInt(environment.getProperty("local.server.port")));
+	
 	System.out.println("Exchange value: "+exchangeValue);
-	exchangeValue.setPort(8000);
+	//exchangeValue.setPort(8000);
 	return exchangeValue;
     }
 
