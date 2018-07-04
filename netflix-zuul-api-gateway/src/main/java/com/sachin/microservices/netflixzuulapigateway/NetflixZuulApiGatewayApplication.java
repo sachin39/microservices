c04e840f1e3aeb56181c1b9b9f6e42a0ff192fb3
Sachin Kumar <Sachin.kr.39@gmail.com>
2018-07-04 12:52:09 +0530

    Created Zuul Api Gateway for logging messages on access 2 microservices .

---
+++ netflix-zuul-api-gateway/src/main/java/com/sachin/microservices/netflixzuulapigateway/NetflixZuulApiGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.sachin.microservices.netflixzuulapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
+
+//Following annotation should be on zuul server
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+@ComponentScan(basePackages="com.sachin.microservices.filter")
+public class NetflixZuulApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixZuulApiGatewayApplication.class, args);
+	}
+}
